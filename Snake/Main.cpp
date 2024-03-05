51b9903563d076e4cc3243319f8c26f26704272b
Kirill Pershin <trasa1111@mail.ru>
2024-03-06 00:33:41 +0300

    save_done

--- Snake/Main.cpp
+++ Snake/Main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <windows.h>
 #include <vector>
+#include <fstream>
+#include <string> 
 void init() {
 	HANDLE stdh = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -12,56 +14,56 @@ void init() {
 	std::cout << board_size_x << " " << board_size_y;
 
 }
-void clear() {
-	COORD topLeft = { 0, 0 };
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO screen;
-	DWORD written;
-
-	GetConsoleScreenBufferInfo(console, &screen);
-	FillConsoleOutputCharacterA(
-		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-	);
-	FillConsoleOutputAttribute(
-		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-	);
-	SetConsoleCursorPosition(console, topLeft);
-}
-void game(HANDLE consoleHandle) {
-	Board board;
-
-	Sleep(2000);
-
-	while (board.snake.live() == 1)
-	{
-		system("cls");
-		board.output();
-		board.change();
-		Sleep(16);
-		SetConsoleCursorPosition(consoleHandle, { 0,0 });
-
-
+void save(int currentSize) {
+	std::string fileText = "2";
+	std::ifstream in("save.out");
+	int cheakRecord = 0;
+	if (in.is_open()) {
+		std::getline(in, fileText);
+		in.close();
+	}
+	std::ofstream out;
+	out.open("save.out");
+	if (currentSize > std::atoi(fileText.c_str())) {
+		out << currentSize;
+		cheakRecord = 1;
+	}
+	else {
+		out << fileText;
+	}
+	out.close();
+	if (cheakRecord) {
+		std::cout << "New Record:"<<currentSize<<'!';
 	}
 }
 
-int main()
-{
-
+void game() {
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO structCursorInfo;
 	GetConsoleCursorInfo(consoleHandle, &structCursorInfo);
 	structCursorInfo.bVisible = FALSE;
 	SetConsoleCursorInfo(consoleHandle, &structCursorInfo);
-
 	while (1) {
-		game(consoleHandle);
-
+		Board board;
+		Sleep(2000);
+		system("cls");
+		while (board.snake.live() == 1)
+		{
+			board.output();
+			board.change();
+			Sleep(16);
+			SetConsoleCursorPosition(consoleHandle, { 0,0 });
+		}
+		system("cls");
 		std::cout << "You are lose!";
-
+		save(board.snake.Pointscoords.size());
 	}
+}
 
-
-
+int main()
+{
+	//init();
+	game();
+	return 0;
 }
 
