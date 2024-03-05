4f0eb900a1444f143fcef3ab83221b5efaa34907
RegreTTO <39791198+RegreTTO@users.noreply.github.com>
2024-03-05 19:12:49 +0300

    Update Apple.cpp

--- Snake/Apple.cpp
+++ Snake/Apple.cpp
@@ -12,7 +12,10 @@ void Apple::randompoint(char** board) {
 		curpointapple.x = rand()%(cst::board_size_x-3);
 		curpointapple.y = rand() % (cst::board_size_y-3);
 
-		if (curpointapple.x != lastpointapple.x and curpointapple.y != lastpointapple.y and board[curpointapple.y][curpointapple.x] == ' ' and curpointapple.x>3 and curpointapple.y > 3) {
+		if (curpointapple.x != lastpointapple.x && 
+		    curpointapple.y != lastpointapple.y && 
+		    board[curpointapple.y][curpointapple.x] == ' ' &&
+		    curpointapple.x>3 && curpointapple.y > 3) {
 			break;
 		}
 	}
